f91a42ec42074353a19b2388c03a5b8f3c4e76f6
Payal Kachhwae <kachhwaepayal@gmail.com>
2023-07-17 17:08:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className = "text-6xl hover:text-blue-500">Hello Everyone!</h1>
+    </div>
+  );
+}
+
+export default App;
